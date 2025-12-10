916b5ddf3f8c2431dc456fa1d34237ec20caef09
HYEALL <kwakse0720@gmail.com>
2025-12-10 15:53:24 +0900

    photo app add layout

---
+++ Test-Project/PhotoApp/PhotoApp/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoApp
+{
+    public partial class PhotoApp : Form
+    {
+        public PhotoApp()
+        {
+            InitializeComponent();
+        }
+    }
+}
