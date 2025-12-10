93fe1e9b196bcaec7a57012f4ec9f414ca544b28
HYEALL <kwakse0720@gmail.com>
2025-12-10 17:18:06 +0900

    button function

--- Test-Project/PhotoApp/PhotoApp/photoApp.cs
+++ Test-Project/PhotoApp/PhotoApp/photoApp.cs
@@ -19,6 +19,7 @@ namespace PhotoApp
 
         private void showButton_Click(object sender, EventArgs e)
         {
+            // 사진 불러오기
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 pictureBox1.Load(openFileDialog1.FileName);
@@ -27,18 +28,29 @@ namespace PhotoApp
 
         private void clearButton_Click(object sender, EventArgs e)
         {
-
+            // 사진 지우기
+            pictureBox1.Image = null;
         }
 
         private void closeButton_Click(object sender, EventArgs e)
         {
-
+            // form 종료
+            this.Close();
         }
 
         private void backgroundButton_Click(object sender, EventArgs e)
         {
-
+            // dialog box 배경색 설정
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.BackColor = colorDialog1.Color;
         }
 
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+        }
     }
 }
